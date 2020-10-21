722772345d49ec520d2f3464f3e134108977e5d4
Thiago Felipe <thiagof.g3@gmail.com>
2020-10-21 12:52:39 -0300

    Acriando classe servico, especializada em buscar dados API

---
+++ app/Services/Milhas123.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Support\Facades\Http;
+
+class Milhas123
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ config/integracao.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Este arquivo deve ser usado para configurações de integrações
+ */
+return [
+    '123_milhas' => [
+        'habilitado' => env('123MILHAS_HABILITADO', false),
+        'url' => env('123MILHAS_API')
+    ]
+];
+
